55ad5bebe9779199dc4c13ddaacd305f5bc2ac19
Kelemen Beáta <90756484+kelemen9@users.noreply.github.com>
2024-03-19 20:49:00 +0100

    beépített fv-ekkel

--- polynomial.R
+++ polynomial.R
@@ -1,87 +1,26 @@
-x <- c(1,2,3,4)
-y <- c(5*x^2+3*x+7)
-n <- length(x)
+#véletlen adatok generálása
+set.seed(123)
+X <- 1:10
+Y <- 5*X^2 + 3*X + rnorm(length(X),  mean=0, sd= 5) 
+#Másodfokú polinom regresszió
+PolynomialRegression <- function (X, Y) {
+	plot (X, Y, main = "Polynomial Regression", xlab = "X", ylab = "Y")
 
-PoliRegres <- function(X, Y) {
+#Másodfokú együttható kiszámítása
+coef <- coefficients(lm(Y - poly(X, 2, raw = TRUE)))
+	a2 <- coef[3]
+	a1 <- coef[2]
+	a0 <- coef[1]
 
-	plot(X, Y)
-	a2 <- calculateA2(X, Y) 
-	a1 <- calculateA1(X, Y)
-	a0 <- calculateA0(X, Y)
-	lines(X, a2 * X^2 + a1 * X + a0, type = "l", lty = 1, col = "red")
+#Kirajzolás
+curve(a2*x^2 + a1*x + a0, add = TRUE, col="blue")
+legend("topleft", legend = c("Data", "Regression"), col = c("black", "blue"), lty = 1)
 
-	cat("a2:", a2, "\n")
-  	cat("a1:", a1, "\n")
-  	cat("a0:", a0, "\n")
-}
+#Együtthatók kiiratása
+cat("A2 (quadratic coefficient): ", a2, "\n")
+cat("A1 (linear coefficient): ", a1, "\n")
+cat("A0 (constant coefficient): ", a0, "\n")
 
-calculateA2 <- function(X, Y) {
+#Futtatás
+PolynomialRegression(X, Y)
 	
-	sumX <- Summary(X)
-      sumY <- Summary(Y)
-      sumXY <- Summary(X,Y)
-	sumX2Y <- Summary(X^2, Y)
-      sumX2 <- Summary(X^2)
-	sumX3 <- Summary(X^3)
-	sumX4 <- Summary(X^4)
-
-	#számláló
-	numerator <- sumX2Y * ((sumX2 * n) - (sumX * sumX)) - sumX3 * ((sumXY * n) - (sumY * sumX)) + sumX2 * ((sumXY * sumX) - (sumY * sumX2))
-	#nevező
-	denominator <- sumX4 * ((sumX2 * n) - (sumX * sumX)) - sumX3 * ((sumX3 * n) - (sumX2  * sumX)) + sumX2 * ((sumX3 * sumX) - (sumX2 * sumX2))
-	return(numerator/denominator)
-}
-
-calculateA1 <- function(X, Y) {
-
-	sumX <- Summary(X)
-      sumY <- Summary(Y)
-      sumXY <- Summary(X,Y)
-	sumX2Y <- Summary(X^2, Y)
-      sumX2 <- Summary(X^2)
-	sumX3 <- Summary(X^3)
-	sumX4 <- Summary(X^4)
-
-	#számláló
-	numerator <- sumX4 * ((sumXY * n) - (sumY * sumX)) - sumX2Y * ((sumX3 * n) - (sumX2 * sumX)) + sumX2 * ((sumX3 * sumY) - (sumX2 * sumXY))
-	#nevező
-	denominator <- sumX4 * ((sumX2 * n) - (sumX * sumX)) - sumX3 * ((sumX3 * n) - (sumX2 * sumX)) + sumX2 * ((sumX3 * sumX) - (sumX2 * sumX2))
-	return(numerator/denominator)
-}
-
-calculateA0 <- function(X, Y) {
-
-	sumX <- Summary(X)
-      sumY <- Summary(Y)
-      sumXY <- Summary(X,Y)
-	sumX2Y <- Summary(X^2, Y)
-      sumX2 <- Summary(X^2)
-	sumX3 <- Summary(X^3)
-	sumX4 <- Summary(X^4)
-
-	#számláló
-	numerator <- sumX4 * ((sumX2 * sumY) - (sumX * sumXY)) - sumX3 * ((sumX3 * sumY) - (sumX2 * sumXY)) + sumX2Y * ((sumX3 * sumX) - (sumX2 * sumX2))
-	#nevező
-	denominator <- sumX4 * ((sumX2 * n) - (sumX * sumX)) - sumX3 * ((sumX3 * n) - (sumX2 * sumX)) + sumX2 * ((sumX3 * sumX) - (sumX2 * sumX2))
-	return(numerator/denominator)
-}
-
-Summary <- function(X, Y) {
-
-	sum <- 0
-	n <- length(X)
-
-    if (missing(Y)) {
-    	for (i in 1:n) {
-      		sum <- sum + X[i]
-    	}
-   		return(sum)
-  	}
-
-	for (i in 1:n) {
-        sum <- sum + X[i] * Y[i]
-    }
-    return(sum)
-}
-
-PoliRegres(x, y)
